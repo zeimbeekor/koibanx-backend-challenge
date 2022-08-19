2f5d3c9ada98562671bccfc3fa90a33fd2e690d3
Alvaro | ZB <zeim.beekor@gmail.com>
2022-08-19 17:33:20 -0500

    chore: add git commit msg linter and updating mongoose to 6.5.2

--- config/default.js
+++ config/default.js
@@ -1,5 +1,5 @@
 module.exports = {
-  'port': 3000,
+  'port': process.env.PORT || 3000,
   'mongodb': {
     'username': process.env.DATABASE_USERNAME,
     'password': process.env.DATABASE_PASSWORD,
