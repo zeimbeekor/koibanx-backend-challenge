bc80d2b2dc84f2735a208099f09b9cb44f8cefac
Alvaro | ZB <zeim.beekor@gmail.com>
2022-08-21 21:23:06 -0500

    chore: add port to app using environment variable

--- config/default.js
+++ config/default.js
@@ -1,11 +1,11 @@
 module.exports = {
-  'port': process.env.PORT || 3000,
-  'mongodb': {
-    'username': process.env.DATABASE_USERNAME,
-    'password': process.env.DATABASE_PASSWORD,
-    'host': process.env.DATABASE_HOST,
-    'port': process.env.DATABASE_PORT,
-    'dbname': process.env.DATABASE_DBNAME,
-    'address': process.env.DATABASE_HOST + ':' + process.env.DATABASE_PORT
+  port: process.env.PORT || 3000,
+  mongodb: {
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    dbname: process.env.DATABASE_DBNAME,
+    address: `${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}`,
   },
 };
